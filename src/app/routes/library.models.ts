c7a306d77c89f3f560c3b4b6f80315aaa07794b1
smile995 <ah.smile995@gmail.com>
2025-09-04 23:17:29 +0600

    Define schema, models and routes for ebook and library

---
+++ src/app/routes/library.models.ts
@@ -0,0 +1,10 @@
+import { LibraryModel } from '../schemaModels/library.model';
+import express from "express";
+const router = express.Router();
+
+// router.post("/register");
+
+
+
+
+export const LibraryRoutes = router;
